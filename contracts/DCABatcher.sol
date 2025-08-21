449f3ad4c5a1e36a1a8d76b4d77f49af6e27db0e
Ronan <155910353+doublerama@users.noreply.github.com>
2025-08-21 19:09:11 +0300

    contract DCABatcher is IAutomationCompatible {

--- contracts/DCABatcher.sol
+++ contracts/DCABatcher.sol
@@ -1,3 +1,8 @@
+interface IAutomationCompatible {
+    function checkUpkeep(bytes calldata) external view returns (bool upkeepNeeded, bytes memory performData);
+    function performUpkeep(bytes calldata performData) external;
+}
+
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
